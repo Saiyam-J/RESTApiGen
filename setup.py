cb7c150d7f88f6d0fa67dbb45927aff12b5674e8
Saiyam-J <saiyamj22@gmail.com>
2023-03-06 21:01:17 +0530

    added description to upload to pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
-#This is the setup file.
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 setup(
     name="RESTApiGen",
-    version="0.1.2",
+    version="0.1.4",
+    url="https://github.com/Saiyam-J/RESTApiGen",
+    author="Saiyam Jain",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[
         "pymysql >= 1.0.2",
         "python_version >= 3.10",
