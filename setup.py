5799f8c161b7f55991785d14679eef6c2e2f0937
Saiyam-J <saiyamj22@gmail.com>
2023-03-16 12:08:31 +0530

    python_requires in setup

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 setup(
     name="RESTApiGen",
-    version="0.1.5",
+    version="0.1.6",
     description="REST API auto-generator.",
     url="https://github.com/Saiyam-J/RESTApiGen",
     author="Saiyam Jain",
     license="MIT",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    python_requires=">=3.10",
     install_requires=[
         "pymysql >= 1.0.2",
-        "python_requires >= 3.10",
         "inflect",
         "flask",
         "flask-sqlalchemy",
